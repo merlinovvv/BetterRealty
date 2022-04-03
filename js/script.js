8c94344955f5b34c61929068c658cc741cc570fb
maerlinovvv <merlinovandrej@gmail.com>
2022-04-04 02:05:18 +0300

    new

--- js/script.js
+++ js/script.js
@@ -44,7 +44,9 @@ if(iconMenu){
 //sales-center
 const salesCenter = document.querySelector('.sales-center');
 if(salesCenter){
-    const btn = document.querySelector('.btn__connect');
+    if(document.querySelector('.btn-connect')){
+        const btn = document.querySelector('.btn-connect');
+    }
     const menusalesCenter = document.querySelector('.black__menu');
     const header = document.querySelector('header');
     const menuBody = document.querySelector('.menu__body');
@@ -52,9 +54,23 @@ if(salesCenter){
         salesCenter.classList.toggle('_active');
         menusalesCenter.classList.toggle('_active');
         header.classList.toggle('_active');
-        btn.classList.toggle('_active');
+        if(btn){
+            btn.classList.toggle('_active');
+        }
         menuBody.classList.toggle('_active');
         iconMenu.classList.toggle('_active');
+        document.body.classList.remove('_lock');
+    });
+};
+const closeMenu = document.querySelector('.menu__close');
+if(closeMenu){
+    const header = document.querySelector('header');
+    const menusalesCenter = document.querySelector('.black__menu');
+    closeMenu.addEventListener("click", function(e){
+        salesCenter.classList.toggle('_active');
+        header.classList.toggle('_active');
+        menusalesCenter.classList.toggle('_active');
+        btn.classList.toggle('_active');
         document.body.classList.remove('_lock');
     });
 };
@@ -71,16 +87,4 @@ if(btn){
 };
 btn.onclick = function() {
     window.scrollTo(scrollX, 0);
-};
-const closeMenu = document.querySelector('.menu__close');
-if(closeMenu){
-    const header = document.querySelector('header');
-    const menusalesCenter = document.querySelector('.black__menu');
-    closeMenu.addEventListener("click", function(e){
-        salesCenter.classList.toggle('_active');
-        header.classList.toggle('_active');
-        menusalesCenter.classList.toggle('_active');
-        btn.classList.toggle('_active');
-        document.body.classList.remove('_lock');
-    });
 };
